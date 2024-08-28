67cb9e4f7a698bf04d43b3ab21d357367525ce9b
Amrutha <16-5581@users.noreply.gitlab.maxvalue.co.in>
2024-08-28 15:43:50 +0530

    multiproviders implemented

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:providercolorapp/controller/home_screen_controller.dart';
+import 'package:providercolorapp/controller/login_screen_controller.dart';
 import 'package:providercolorapp/view/home_screen/HomeScreen.dart';
 
 void main()
@@ -16,8 +17,22 @@ class MyApp extends StatelessWidget
   @override
   Widget build(BuildContext context)
   {
-    return ChangeNotifierProvider(
-      create: (context) => HomeScreenController(),
+    //Single Provider
+    // return ChangeNotifierProvider(
+    //   create: (context) => HomeScreenController(),
+    //   child: const MaterialApp(
+    //     debugShowCheckedModeBanner: false,
+    //     title: 'Flutter Demo',
+    //     home: HomeScreen(),
+    //   ),
+    // );
+    
+    //MultiProvider
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => HomeScreenController()),
+        ChangeNotifierProvider(create: (context) => LoginScreenController()),
+      ],
       child: const MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
